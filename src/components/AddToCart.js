6aee5928d74c03c0d3ce658cd9aebb259a783c9e
Alberto <albertovinicius3@gmail.com>
2022-03-20 16:29:53 -0300

    add create new product function

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,10 +1,19 @@
+import { useProducts } from '../hooks/useProducts';
+
 import iconCart from '../assets/images/icon-cart-white.svg';
 
 import styles from '../styles/AddToCart.module.scss';
 
-function AddToCart() {
+function AddToCart({count}) {
+
+  const { products } = useProducts()
+
+  function createNewProduct() {
+    products.push({name: "Fall Limited Edition Sneakers", count: count});
+  }
+
   return (
-    <button className={styles.addToCart}>
+    <button className={styles.addToCart} onClick={(e) => createNewProduct()}>
       <img src={iconCart} alt='Cart' />
       Add to cart
     </button>
