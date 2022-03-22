ce01927719ea7aaf06afb419bed743f170f558f9
Alberto Albuquerque <89992304+allbertuu@users.noreply.github.com>
2022-03-22 01:08:05 -0300

    add logic to avoid duplicate products

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,20 +6,30 @@ import styles from '../styles/AddToCart.module.scss';
 
 function AddToCart({count, setCount}) {
 
-  const { products } = useProducts()
+  const { products } = useProducts();
 
-  function addToCart() {
+  function addToCart(productToAdd) {
     if (count === 0) {
       return;
     }
-    products.push({name: "Fall Limited Edition Sneakers", count: count});
+    
+    let productToUpdate = products.findIndex((product) => {
+      return product.name === productToAdd;
+    });
+    if (productToUpdate === -1) {
+      products.push({name: "Fall Limited Edition Sneakers", count: count});
+    }
+    else {
+      products[productToUpdate].count = count;
+    }
+
     // se o produto tem o mesmo nome, altera somente a contagem do mesmo
     setCount(0);
     // um modal de adicionado com sucesso
   }
 
   return (
-    <button className={styles.addToCart} onClick={(e) => addToCart()}>
+    <button className={styles.addToCart} onClick={(e) => addToCart("Fall Limited Edition Sneakers")}>
       <img src={iconCart} alt='Cart' />
       Add to cart
     </button>
