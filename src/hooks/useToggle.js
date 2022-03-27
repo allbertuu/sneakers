99c0d6ee5d01d18b0d37147a457dbfa9df65f7e7
Alberto <albertovinicius3@gmail.com>
2022-03-27 14:13:52 -0300

    fix scrollbar

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -3,6 +3,5 @@ import { useCallback, useState } from 'react';
 export default function useToggle(initialState = false) {
   const [state, setState] = useState(initialState);
   const toggle = useCallback(() => setState(state => !state), []);
-  state ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
   return [state, toggle];
 }
