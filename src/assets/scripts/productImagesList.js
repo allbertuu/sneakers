6a009d101772ac5562c7596720acd527dfea2e6c
Alberto <albertovinicius3@gmail.com>
2022-04-10 18:18:42 -0300

    add js in modal of products and doing clean code

---
+++ src/assets/scripts/productImagesList.js
@@ -0,0 +1,6 @@
+import img1 from '../images/image-product-1.jpg';
+import img2 from '../images/image-product-2.jpg';
+import img3 from '../images/image-product-3.jpg';
+import img4 from '../images/image-product-4.jpg';
+
+export const productImagesList = [img1, img2, img3, img4];
--- src/components/layout/Product.js
+++ src/components/layout/Product.js
@@ -1,91 +1,77 @@
 import { useState } from 'react';
 import useToggle from '../../hooks/useToggle';
 
-import img1_small from '../../assets/images/image-product-1-thumbnail.jpg';
-import img1 from '../../assets/images/image-product-1.jpg';
-import img2_small from '../../assets/images/image-product-2-thumbnail.jpg';
-import img2 from '../../assets/images/image-product-2.jpg';
-import img3_small from '../../assets/images/image-product-3-thumbnail.jpg';
-import img3 from '../../assets/images/image-product-3.jpg';
-import img4_small from '../../assets/images/image-product-4-thumbnail.jpg';
-import img4 from '../../assets/images/image-product-4.jpg';
-
+//scripts
+import { productImagesList } from '../../assets/scripts/productImagesList';
+//icons
 import CloseIcon from '@mui/icons-material/Close';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-
-// import iconClose from '../../assets/images/icon-close-white.svg';
-
+//styles
 import styles from '../../styles/Product.module.scss';
 
 function Product() {
 
-  const [mainImg, setMainImg] = useState(img1);
-
-  function handleImg(img) {
-    setMainImg(img);
-    // aplicar estilos de selecionado à imagem menor que foi selecionada
-  }
+  const [mainImg, setMainImg] = useState(productImagesList[0]);
 
   const [isOpen, toggle] = useToggle();
 
-  function setSelected(img) {
-    if (img === mainImg) {
-      return styles.selected;
+  function setSelectedImg(img) {
+    return img === mainImg ? styles.selected : undefined;
+  }
+
+  function prev() {
+    let firstImage = productImagesList[0];
+    if(mainImg === firstImage) {
+      return;
     }
-    else {
-      return undefined;
+    const currentImg = productImagesList.indexOf(mainImg)
+    setMainImg(productImagesList[currentImg - 1])
+  }
+
+  function next() {
+    let lastImage = productImagesList[productImagesList.length - 1];
+    if(mainImg === lastImage) {
+      return;
     }
+    const currentImg = productImagesList.indexOf(mainImg)
+    setMainImg(productImagesList[currentImg + 1])
   }
 
   return (
     <>
       <section className={styles.container}>
-        <img className={styles.main_img} src={mainImg} alt="Main product" onClick={(e) => toggle()} />
+        <img className={styles.main_img} src={mainImg} alt="Main product" onClick={() => toggle()} />
         <div className={styles.l_product_images}>
-          <div onClick={(e) => handleImg(img1)}>
-            <img src={img1_small} alt="Product" className={setSelected(img1)} />
-          </div>
-          <div onClick={(e) => handleImg(img2)}>
-            <img src={img2_small} alt="Product" className={setSelected(img2)} />
-          </div>
-          <div onClick={(e) => handleImg(img3)}>
-            <img src={img3_small} alt="Product" className={setSelected(img3)} />
-          </div>
-          <div onClick={(e) => handleImg(img4)}>
-            <img src={img4_small} alt="Product" className={setSelected(img4)} />
-          </div>
+          {productImagesList.map((srcProductImage, index) => (
+            <div onClick={() => setMainImg(srcProductImage)} key={index}>
+              <img src={srcProductImage} alt="Product view" className={setSelectedImg(srcProductImage)} />
+            </div>
+          ))}
         </div>
       </section>
 
       {isOpen &&
         <div aria-hidden="true" className={styles.l_modal}>
           <section className={styles.container_modal}>
-            <button className="lg:-mb-4 text-right" onClick={(e) => toggle()}>
+            <button className="lg:-mb-4 text-right" onClick={() => toggle()}>
               <CloseIcon fontSize='large' />
             </button>
             <div className={styles.l_main_img}>
-              <button className={styles.prev}>
+              <button className={styles.prev} onClick={() => prev()}>
                 <ArrowBackIosNewIcon />
               </button>
               <img className={styles.main_img} src={mainImg} alt="Main product" />
-              <button className={styles.next}>
+              <button className={styles.next} onClick={() => next()}>
                 <ArrowForwardIosIcon />
               </button>
             </div>
             <div className={styles.l_product_images_modal}>
-              <div onClick={(e) => handleImg(img1)}>
-                <img src={img1_small} alt="Product" className={setSelected(img1)} />
-              </div>
-              <div onClick={(e) => handleImg(img2)}>
-                <img src={img2_small} alt="Product" className={setSelected(img2)} />
-              </div>
-              <div onClick={(e) => handleImg(img3)}>
-                <img src={img3_small} alt="Product" className={setSelected(img3)} />
-              </div>
-              <div onClick={(e) => handleImg(img4)}>
-                <img src={img4_small} alt="Product" className={setSelected(img4)} />
-              </div>
+              {productImagesList.map((srcProductImage, index) => (
+                <div onClick={() => setMainImg(srcProductImage)} key={index}>
+                  <img src={srcProductImage} alt="Product view" className={setSelectedImg(srcProductImage)} />
+                </div>
+              ))}
             </div>
           </section>
         </div>
