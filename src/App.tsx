8cc67b7da50d2db2b47764ccf54a5fe6b5be40ef
Alberto <albertovinicius3@gmail.com>
2022-06-29 13:39:58 -0300

    refactor(components): create a own folder to every component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // components
-import Header from "./components/layout/Header";
-import Product from "./components/layout/Product";
-import Description from "./components/layout/Description";
-import Footer from "./components/layout/Footer";
+import Header from "./components/Header";
+import Product from "./components/Product";
+import Description from "./components/Description";
+import Footer from "./components/Footer";
 // styles
 import "./sass/App.scss";
 
