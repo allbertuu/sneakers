9ff181ef40461b8df2eb3580547e490b37ee8a73
Alberto <albertovinicius3@gmail.com>
2022-07-04 11:15:31 -0300

    refactor(theme): create colors and theme to styled-components
    removing the dependence of scss files

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: "hsl(26, 100%, 55%)",
+  primaryDark: "hsl(26, 96%, 52%)",
+  primaryLight: "hsl(26, 98%, 60%)",
+  primaryLighter: "hsl(25, 100%, 94%)",
+  secondary: "hsl(220, 14%, 75%)",
+  secondaryDark: "hsl(219, 9%, 45%)",
+  secondaryDarker: "hsl(220, 13%, 13%)",
+  secondaryLight: "hsl(223, 64%, 98%)",
+  black75: "hsla(0, 0%, 0%, 0.75)",
+};
